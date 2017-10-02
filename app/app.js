65f76f99ea4602be31422af598fbb4d01aa4139c
languyen <wildboar.work@gmail.com>
2017-10-02 22:13:25 +0700

    Question 5 and chat UI

--- app/app.js
+++ app/app.js
@@ -4,22 +4,28 @@
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/customers',
-            {
-                controller: 'CustomersController',
-                templateUrl: '/app/views/customers.html'
-            })
+        {
+            controller: 'CustomersController',
+            templateUrl: '/app/views/customers.html'
+        })
         //Define a route that has a route parameter in it (:customerID)
         .when('/customerorders/:customerID',
-            {
-                controller: 'CustomerOrdersController',
-                templateUrl: '/app/views/customerOrders.html'
-            })
-        //Define a route that has a route parameter in it (:customerID)
+        {
+            controller: 'CustomerOrdersController',
+            templateUrl: '/app/views/customerOrders.html'
+        })
+        //Define a route that has a route parameter in it (:orders)
         .when('/orders',
-            {
-                controller: 'OrdersController',
-                templateUrl: '/app/views/orders.html'
-            })
+        {
+            controller: 'OrdersController',
+            templateUrl: '/app/views/orders.html'
+        })
+        //Define a route that has a route parameter in it (:chat)
+        .when('/chat',
+        {
+            controller: 'OrdersController',
+            templateUrl: '/app/views/chat.html'
+        })
         .otherwise({ redirectTo: '/customers' });
 });
 
